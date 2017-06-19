3852126e692becf02448956d7f54efccac3c9004
VinceW <info@vincew.net>
2017-06-19 21:59:22 +0200

    fixed some typos

--- yawap.sh
+++ yawap.sh
@@ -9,10 +9,10 @@ AP_DEVICE="lo"
 #AP_DEVICE="wlan0"
 #AP_DEVICE="wlan1"
 
-# WLAN_DEVICE
-# Uncomment th device on which Internet is available
-WLAN_DEVICE="eth0"
-#WLAN_DEVICE="eth1"
+# WAN_DEVICE
+# Uncomment the device on which Internet is available
+WAN_DEVICE="eth0"
+#WAN_DEVICE="eth1"
 
 DNSMASQ_CONF="/etc/dnsmasq.conf"
 HOSTAPD_CONF="/etc/hostapd/hostapd.conf"
@@ -41,7 +41,7 @@ echo "# YaWaP Configuration" >> $DNSMASQ_CONF
 echo "log-facility=/var/log/dnsmasq.log" >> $DNSMASQ_CONF
 echo "#address=/#/10.0.0.1" >> $DNSMASQ_CONF
 echo "#address=/google.com/10.0.0.1" >> $DNSMASQ_CONF
-echo "interface=wlan0" >> $DNSMASQ_CONF
+echo "interface="$AP_DEVICE >> $DNSMASQ_CONF
 echo "dhcp-range=10.0.0.10,10.0.0.250,12h" >> $DNSMASQ_CONF
 echo "dhcp-option=3,10.0.0.1" >> $DNSMASQ_CONF
 echo "dhcp-option=6,10.0.0.1" >> $DNSMASQ_CONF
@@ -64,10 +64,14 @@ ifconfig $AP_DEVICE 10.0.0.1/24
 # Setup iptables
 iptables -t nat -F
 iptables -F
-iptables -t nat -A POSTROUTING -o $WLAN_DEVICE -j MASQUERADE
-iptables -A FORWARD -i $AP_DEVICE -o $WLAN_DEVICE -j ACCEPT
+iptables -t nat -A POSTROUTING -o $WAN_DEVICE -j MASQUERADE
+iptables -A FORWARD -i $AP_DEVICE -o $WAN_DEVICE -j ACCEPT
 echo '1' > /proc/sys/net/ipv4/ip_forward
 
+
+echo .
+echo "Run following commands in seperate terminal windows."
+echo .
 echo dnsmasq -C /etc/dnsmasq.conf -d
 echo hostapd /etc/hostapd/hostapd.conf
 
